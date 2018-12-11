9b8cd6cb716358698c078a96ad64bbf1f5989007
Tom King <design.tomking@gmail.com>
2018-12-11 09:09:04 -0700

    Test keyframes on block component

--- src/components/Block/index.js
+++ src/components/Block/index.js
@@ -2,20 +2,28 @@ import React from 'react';
 import injectSheet from 'react-jss';
 
 const styles = {
+  '@keyframes slideRight': {
+    to: {transform: 'translate(0, 10px)'}
+  },
   block: {
     display: 'block',
     width: 10,
     height: 10,
     background: 'cadetblue',
-    transition: 'boxShadow 0.5s ease-in-out, background 0.5s ease-in-out',
+    transition: 'boxShadow 0.5s ease-in-out, background 0.5s ease-in-out, animation 5s linear',
+    
     '&:hover': {
+      animationName: 'slideRight',
+      animationDuration: '0.5s',
       background: 'deeppink',
       boxShadow: '0 1px 1px #777',
       zIndex: 10,
       transition: 'none',
-    }
+    },
   }
 }
+
+
 
 const Block = ({ classes, children }) => (
   <div className={classes.block}>
